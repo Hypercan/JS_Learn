7203245a0e86e0aeeb0259fabc149cd7f9abd7a0
Hypercan <kanadafirat@gmail.com>
2022-10-05 00:27:22 +0300

    Fixed the Lines

--- hello_world.js
+++ hello_world.js
@@ -1,3 +1,4 @@
+// The variable declarations.
 // const {constant} bu değişkeni sadece birkez tanımlanır ve değiştirilemez.
 // let if ile tanımlanırsa sadce oradan onun dışında değiştirilebilir.
 // var değiştirilebilir global scope veya function scope c# daki private veya public gibi.
@@ -5,12 +6,15 @@
 console.log("Hello World!");
 let serverName = "google.com";
 console.log(serverName);
+
 var myName = "Firat Can";
 console.log(myName)
 console.log("Şimdi soyadını ekliyorum bakalım olacak mı.")
+
 //  BU ilk yol
 myName = myName + " Karakoc";
 console.log(myName)
 // Bu ikinci yol
+
 myName += " Karakoc";
 console.log(myName)
