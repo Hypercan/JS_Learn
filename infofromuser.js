fa6fd3c4f2067e17301c092d45e04ba6b9e5c3b5
Hypercan <kanadafirat@gmail.com>
2022-10-06 23:40:27 +0300

    Liste içi seçim oluşturuldu

---
+++ infofromuser.js
@@ -0,0 +1,7 @@
+// **** Liste içerisindeki elemana ulaşmak veya eleman eklemek
+
+let sonOge = document.querySelector("#unlist>li#list:last-child")
+sonOge.innerHTML = "son oge degistirildi"
+
+let ilkOge = document.querySelector("#unlist>li#list:first-child")
+ilkOge.innerHTML = "ilk oge degistirildi" 
