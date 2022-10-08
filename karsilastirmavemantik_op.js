7f40c272e021b1c1d153714f5bff1c21a3bc9e94
Hypercan <kanadafirat@gmail.com>
2022-10-09 00:01:53 +0300

    yeni bölüm

---
+++ karsilastirmavemantik_op.js
@@ -0,0 +1,30 @@
+// KARŞILAŞTIRMA OPERATÖRLERİ
+// Eşitse * == *
+var a = 10;
+var b = "10";
+console.log(a == b) //cevap true
+
+// === hem değeri hemde türü eşitse yani string === string, int === int
+var c = 25;
+var d = "25";
+console.log(c === d); //cevap false çünkü var c int , var d ise string
+
+//  =! eşit değilse işareti 
+// eğer a ile b eşit degilse true ver
+console.log(a != b) // false çünkü eşit 
+console.log(a !== b) //burda kontrol ettiği hem türü hem değeri cevap ->  true çünkü rakam aynı fakat türü farklı 
+console.log(a != c) // burda direk true geliyor çünkü değer farklı
+
+// < küçük işareti 
+console.log(a < b) // false
+console.log(a < c) //true
+
+// <= küçük veya eşitse
+// > büyükse >= büyükse veya eşitse
+
+// MANTIKSAL OPERATÖRLER
+//  && VE
+console.log(a < c && c != b) // cevap true 
+
+// || veya 
+console.log()
