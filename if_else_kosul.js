abce68c51add3aeeb0d86a3a7e5d222b5aed58ae
Hypercan <kanadafirat@gmail.com>
2022-10-09 22:33:40 +0300

    lecture end

---
+++ if_else_kosul.js
@@ -0,0 +1,11 @@
+// If / Else koşul yapıları
+
+var a = 15;
+var b = 20;
+var c = "15";
+
+if (a < b) {
+    console.log(a + " sayısı " + b + " sayısından küçüktür")
+} else {
+    console.log(a + " sayısı " + b + " sayısından büyüktür")
+}
